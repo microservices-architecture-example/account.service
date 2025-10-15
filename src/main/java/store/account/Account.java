e44f029c5b9249d95035025a0cc6339e1757cce8
luigilopesz <luigilopes09@gmail.com>
2025-10-15 16:22:11 -0300

    account fix (now mapping db names)

--- src/main/java/store/account/Account.java
+++ src/main/java/store/account/Account.java
@@ -1,16 +1,31 @@
 package store.account;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Builder @Data @Accessors(fluent = true, chain = true)
+@Entity
+@Table(name = "account", schema = "account")
 public class Account {
 
+    @Id
+    @Column(name = "id_account")
     private String id;
+
+    @Column(name = "tx_name")
     private String name;
+
+    @Column(name = "tx_email")
     private String email;
+
     private String password;
+
+    @Column(name = "tx_sha256")
     private String sha256;
     
 }
